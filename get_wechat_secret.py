604caa1a9cf5612b89fc41b62fd6001f4cf8f093
iWorkers <75261857+iWorkers@users.noreply.github.com>
2023-06-17 15:01:45 +0800

    Add files via upload

---
+++ get_wechat_secret.py
@@ -0,0 +1,77 @@
+import copy
+import time
+from io import BytesIO
+from PIL import Image
+import qrcode
+from pyzbar.pyzbar import decode
+from http_utils import HTTPClient
+from url_conf import URLS
+from LgbConfig import SHOW_WECHAT_QRCODE
+import requests
+
+def get_tok_uid():
+    http_client = HTTPClient()
+    res_text = http_client.send(URLS['wexin_request_qrcode'])
+    assert isinstance(res_text, str), "错误：" + URLS['wexin_request_qrcode']
+    key_str = '<img class="web_qrcode_img" src="/'
+    idx = res_text.find(key_str)
+    assert idx != -1, "没有找到微信提供的二维码图片"
+
+    qrcode_href = res_text[idx:idx+70].split('"')[3]
+    qrcode_uuid = qrcode_href.split('/')[-1]
+    # print(qrcode_href, qrcode_uuid)
+
+    qrcode_url = copy.deepcopy(URLS['wexin_request_uuid'])
+    qrcode_url['req_url'] += qrcode_href
+
+    res_content = http_client.send(qrcode_url)
+    assert isinstance(res_text, str), "错误：" + qrcode_url
+    image = Image.open(BytesIO(res_content))
+    barcodes = decode(image)
+    for barcode in barcodes:
+        barcode_url = barcode.data.decode("utf-8")
+    qr = qrcode.QRCode()
+    barcode_url = URLS['wexin_confirm_request_uuid']['req_url'] + qrcode_uuid
+    qr.add_data(barcode_url)
+    #qr.print_ascii(invert=True)
+    #print(qrcode_url['req_url'])
+    if SHOW_WECHAT_QRCODE:
+        img = qr.make_image()
+        img.show()
+    
+    scan_qrcode_sucess = False
+    wexin_poll_confirm_request_uuid_url = copy.deepcopy(URLS['wexin_poll_confirm_request_uuid'])
+    for _ in range(100):
+        time.sleep(1)
+        current_milli_time = lambda: str(int(round(time.time() * 1000)))
+        wexin_poll_confirm_request_uuid_url['req_url'] += qrcode_uuid + '&_=' + current_milli_time()
+        res_text = http_client.send(wexin_poll_confirm_request_uuid_url)
+        if not isinstance(res_text, str): continue
+        res = res_text.find('window.wx_errcode=405;window.wx_code=')
+        if res == -1:
+            continue
+
+        scan_qrcode_sucess = True
+        wx_code = res_text.split('=')[-1]
+        wx_code = wx_code.replace('\'', '')
+        wx_code = wx_code.replace(';', '')
+        break
+    assert scan_qrcode_sucess, "微信扫码超时,退出登录"
+
+    get_log2023_uid_tok_url = copy.deepcopy(URLS['lgb2023_login'])
+    get_log2023_uid_tok_url['req_url'] += wx_code + get_log2023_uid_tok_url['req_url_suffix']
+    token_dict = http_client.send(get_log2023_uid_tok_url)
+    status = token_dict.get("status")
+    if status == 20000 and 'data' in token_dict:
+        token = token_dict.get("data").get("uidtok")
+        memberId = token_dict.get("data").get("unionId")
+        cookie='acw_tc='+requests.utils.dict_from_cookiejar(http_client.get_cookies())['acw_tc']
+        user_info = {'memberId': memberId, 'token': token, 'cookie': 'acw_tc='+requests.utils.dict_from_cookiejar(http_client.get_cookies())['acw_tc']}
+        print("微信登录LGBtoken:", user_info)
+        print("登录成功")
+        
+        
+        return memberId, token,cookie
+    print(token_dict)
+    print("登录失败")
+    return 'memberId', 'token'
---
+++ main.py
@@ -0,0 +1,117 @@
+#pyinstaller -F -c main.py
+#nuitka --mingw64 --standalone --show-progress --show-memory --output-dir=out --onefile main.py
+from get_wechat_secret import get_tok_uid
+from http_utils import HTTPClient
+from url_conf import URLS
+import json
+from LgbConfig import MIN_TIME, MAX_TIME
+import datetime
+import random
+import time
+import logging
+import traceback
+import string
+
+logging.basicConfig(filename='log.txt',
+                    level=logging.DEBUG,
+                    filemode='a+',
+                    format='[%(asctime)s] [%(levelname)s] >>> \n%(message)s',
+                    datefmt='%Y-%m-%d %I:%M:%S')
+
+try:
+    memberId, token ,cookie= get_tok_uid()
+    user_info = {'memberId': memberId, 'token': token,'cookie': cookie}
+    with open('mul_token.txt', 'a', encoding='utf-8') as f:
+        f.write(str(user_info) + '\n')
+    http_client = HTTPClient()
+    http_client.memberId=memberId
+    http_client.token=token
+    http_client.cookie=cookie
+
+
+
+    flag=True
+
+    while flag :
+        
+        zl=input("【查询积分按c  |  答题按a  |  抽奖按m   |   退出按q  】 :").upper()
+        if zl == 'C':
+        
+            result_dict = http_client.send(URLS['lgb2023_competition'])
+            print('当前积分为：'+str(result_dict['data']['points']))
+            print('---------------------------------------------------')
+        if zl == 'A':
+            with open("answerdict.json", 'r', encoding='utf-8') as fw:
+                right_answer = json.load(fw)
+            result_dict = http_client.send(URLS['lgb2023_start'])
+            
+            if '成功' not in str(result_dict):
+                if '挑战已完成' in str(result_dict):
+                    print('今天已答题，明天再来!')
+                print('---------------------------------------------------')
+                continue
+            flag3=True
+            while flag3:
+                qus=result_dict['data']['ques']['content']
+                qus_id=result_dict['data']['ques']['quesId']
+                qus_type=result_dict['data']['ques']['quesTypeStr']
+                qus_no=result_dict['data']['ques']['quesNo']
+                qus_option=result_dict['data']['ques']['options']
+                answer=[]
+                print(str(qus_no)+":"+qus)
+                j=0
+                xuhao=list(string.ascii_uppercase)
+                answer_list={}
+                for res in qus_option:
+                    print(xuhao[j]+"、"+res)
+                    answer_list[xuhao[j]]=res
+                    j=j+1
+                if qus in right_answer.keys():
+                    
+                    answer = list(right_answer[qus])
+                    data1=json.dumps({"quesId":"%s" % qus_id,"answerOptions":answer})
+                else:
+                
+                    if '多选' in qus_type:
+                        
+                        temp_ans=[]
+                        zl=input("多选题连续输入答案序号如ABC按enter提交！").upper()
+                        for al in list(zl):
+                            temp_ans.append(answer_list[al])
+                        answer =list(temp_ans)
+                        data1=json.dumps({"quesId":"%s" % qus_id,"answerOptions":answer})
+                    else:
+                        zl=input("单选题输入答案序号如A按enter提交！").upper()
+                        answer=list(answer_list[zl])
+                        data1=json.dumps({"quesId":"%s" % qus_id,"answerOptions":answer})
+                #print(data1)
+                
+                result_dict = http_client.send(URLS['lgb2023_answer'],data=data1)
+                if qus_no == 5:
+                    result = http_client.send(URLS['lgb2023_submitcompetition'])
+                    print('---------------------------------------------------')
+                    print("答题结束！")
+                    print('本次答对题目数：', result.get('data', {}).get("correctNum"))
+                    flag3=False
+        if zl == 'M':
+            result_dict = http_client.send(
+                URLS['getdrawsurplusnum'])
+            surplusNum = int(result_dict['data']['surplusNum'])
+            if surplusNum == 0:
+                print("您的抽奖次数已用完！")
+            for i in range(surplusNum):
+                result_dict = http_client.send(URLS['drawprize'])
+                data = result_dict['data']
+                if not data:  # 无数据
+                    print("您的抽奖次数已用完！")
+                    print('---------------------------------------------------')
+                    continue
+                prizeName = data['prizeName']
+                print('第%s次抽奖获得:' % (i+1), prizeName)
+                time.sleep(random.randint(MIN_TIME, MAX_TIME))
+            print('---------------------------------------------------')
+        if zl == 'Q':
+            flag=False
+except Exception as e:
+    traceback.print_exc() # 打印到控制台
+    logging.error(traceback.format_exc())
